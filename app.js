4d3fd93c7011b512f83a47e831a87bf1fc0bbb0e
jhs512 <jangka512@gmail.com>
2022-03-13 17:40:51 +0900

    게시물리스트

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mysql from "mysql2/promise";
+
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "express_sample1",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
 const app = express();
 const port = 3000;
 
@@ -6,11 +18,12 @@ app.get("/", (req, res) => {
   res.send("안녕하세요.");
 });
 
-app.get("/user-info", (req, res) => {
-  res.json({
-    id: 1,
-    username: "홍길동",
-  });
+app.get("/articles", async (req, res) => {
+  const [rows] = await pool.query(
+    "SELECT * FROM article ORDER BY id DESC LIMIT 2"
+  );
+
+  res.json(rows);
 });
 
 app.listen(port, () => {
