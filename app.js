8376d290d88b8557aa39d67afc656a7c38783fee
jhs512 <jangka512@gmail.com>
2022-03-13 17:21:33 +0900

    json 응답

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("ㅋㅋㅋ");
+  res.send("안녕하세요.");
+});
+
+app.get("/user-info", (req, res) => {
+  res.json({
+    id: 1,
+    username: "홍길동",
+  });
 });
 
 app.listen(port, () => {
